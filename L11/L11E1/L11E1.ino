3fab8028d22a1394e4a87a946fc8d8ac5cffa884
Krishna Karthik Peri <krishnakarthik1909@gmail.com>
2025-07-02 16:18:15 +0200

    Added L11-1

---
+++ L11/L11E1/L11E1.ino
@@ -0,0 +1,73 @@
+/*
+    Arduino and ADXL345 Accelerometer Tutorial
+     by Dejan, https://howtomechatronics.com
+*/
+
+#include <Server.h>
+#include <Wire.h>  // Wire library - used for I2C communication
+int ADXL345 = 0x53; // The ADXL345 sensor I2C address
+int servoMotor = 10;
+Servo myservo.attach(servoMotor); // Attach the servo motor to pin 10
+float X_out, Y_out, Z_out;  // Outputs
+
+void setup() {
+  Serial.begin(9600); // Initiate serial communication for printing the results on the Serial monitor
+  Wire.begin(); // Initiate the Wire library
+  // Set ADXL345 in measuring mode
+  Wire.beginTransmission(ADXL345); // Start communicating with the device 
+  Wire.write(0x2D); // Access/ talk to POWER_CTL Register - 0x2D
+  // Enable measurement
+  Wire.write(8); // (8dec -> 0000 1000 binary) Bit D3 High for measuring enable 
+  Wire.endTransmission();
+  delay(10);
+
+  //X-axis
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x1E);  // X-axis offset register
+  Wire.write(-2);
+  Wire.endTransmission();
+  delay(10);
+
+  //Y-axis
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x1F); // Y-axis offset register
+  Wire.write(-1);
+  Wire.endTransmission();
+  delay(10);
+
+
+  //Z-axis
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x20); // Z-axis offset register
+  Wire.write(+1);
+  Wire.endTransmission();
+  delay(10);
+
+}
+
+void loop() {
+  // === Read acceleromter data === //
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x32); // Start with register 0x32 (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(ADXL345, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  X_out = ( Wire.read()| Wire.read() << 8); // X-axis value
+  X_out = X_out/256; //For a range of +-2g, we need to divide the raw values by 256, according to the datasheet
+  Y_out = ( Wire.read()| Wire.read() << 8); // Y-axis value
+  Y_out = Y_out/256;
+  Z_out = ( Wire.read()| Wire.read() << 8); // Z-axis value
+  Z_out = Z_out/256;
+
+  if(sqrt(X_out*X_out + Y_out*Y_out + Z_out*Z_out) < 0.3){
+    myservo.write(180); // If the total acceleration is less than 1.5g, set servo to 180 degrees
+  } else {
+    myservo.write(0); // Otherwise, set servo to 0 degrees
+  }
+  Serial.print("Xa= ");
+  Serial.print(X_out);
+  Serial.print("   Ya= ");
+  Serial.print(Y_out);
+  Serial.print("   Za= ");
+  Serial.println(Z_out);
+
+}
